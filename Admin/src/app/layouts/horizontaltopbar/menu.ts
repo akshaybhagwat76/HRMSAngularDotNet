2b306015da2f1e1f9af3e74ab3da530da088b9b8
ttnnphuoc <60534054+ttnnphuoc@users.noreply.github.com>
2021-07-06 10:44:40 +0700

    add source admin
    add source admin

---
+++ Admin/src/app/layouts/horizontaltopbar/menu.ts
@@ -0,0 +1,15 @@
+import { MenuItem } from './menu.model';
+
+export const MENU: MenuItem[] = [
+    {
+        id: 1,
+        label: 'MENUITEMS.DASHBOARDS.TEXT',
+        icon: 'bx-home-circle',
+    },
+    {
+        id: 6,
+        label: 'MENUITEMS.UIELEMENTS.TEXT',
+        icon: 'bx-tone',
+    }
+];
+
---
+++ Admin/src/app/pages/dashboards/default/default.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { transactions, statData } from './data';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.scss']
+})
+export class DefaultComponent implements OnInit {
+
+  validationform: FormGroup; // bootstrap validation form
+
+  // Form submition
+  submit: boolean;
+
+  transactions;
+  statData;
+  @ViewChild('content') content;
+
+  constructor(private modalService: NgbModal,public formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+        /**
+     * Bootstrap validation form data
+     */
+         this.validationform = this.formBuilder.group({
+          firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          employeeCode: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          company: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          category: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          department: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          project: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          dateOfJoining: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          mobileNo: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          employeeType: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          maritalStatus: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          physicalNo: [''],
+          religion: ['',[Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          nationality: ['',[Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+          biometricCode: [''],
+          higherAuthorityName: [''],
+          higherAuthority: [''],
+          higherAuthorityProject: [''],
+          emailAddress: [''],
+          thirdPartyType: [''],
+          thirdParty: [''],
+          workingStatus: [''],
+          probatlonPerlod: [''],
+          refName: [''],
+          refNo: [''],
+          bloodGroup: [''],
+          cast: [''],
+          dateOfBirth: [''],
+        });
+
+    /**
+     * Fetches the data
+     */
+    this.fetchData();
+  }
+  ngAfterViewInit() {
+    // setTimeout(() => {
+    //   this.openModal();
+    // }, 2000);
+  }
+  /**
+   * Fetches the data
+   */
+  private fetchData() {
+    this.transactions = transactions;
+    this.statData = statData;
+  }
+
+  // openModal() {
+  //   this.modalService.open(this.content, { centered: true });
+  // }
+
+  /**
+   * Returns form
+   */
+   get form() {
+     console.log(this.validationform.controls);
+    return this.validationform.controls;
+  }
+
+    /**
+   * Bootsrap validation form submit method
+   */
+     validSubmit() {
+      this.submit = true;
+    }
+}

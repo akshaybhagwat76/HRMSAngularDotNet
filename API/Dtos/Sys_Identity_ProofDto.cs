e8b7d20ffd8dccd8ebd1525c9426d3c8c57b09c0
Praveen jha <praveenkumarjha21@gmail.com>
2021-07-28 11:37:07 +0530

    Created API's (Identity Proof,User type , Other information)

---
+++ API/Dtos/Sys_Identity_ProofDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class Sys_Identity_ProofDto : BaseDto
+    {
+        public string Identity_Type { get; set; }
+        public string Identity_No { get; set; }
+        public IFormFile Attachments { set; get; }
+        public string Attachments_File_Name { get; set; }
+        public DateTime Valid_Upto { get; set; }
+    }
+}
